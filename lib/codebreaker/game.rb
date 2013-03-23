ebe672f294343ffb5408755cebe50bb8f66546ab
kostja <konstantin@silin.info>
2013-03-23 17:44:57 +0100

    added first tests for guessing

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -3,9 +3,20 @@ module Codebreaker
     def initialize(output)
       @output = output
     end
-    def start
+
+    def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
+
+    def guess(guess)
+      if @secret.include?(guess[0])
+        @output.puts '-'
+      else
+        @output.puts ''
+      end
+    end
+
   end
 end
