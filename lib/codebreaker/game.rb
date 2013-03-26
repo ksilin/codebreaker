c54d4fa4b558d3f7e292a898faecb447e92313da
kostja <konstantin@silin.info>
2013-03-26 18:46:21 +0100

    finished and refactored guess feature

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,25 +11,36 @@ module Codebreaker
     end
 
     def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-      if exact_match?(guess, index)
-        mark << '+'
-      elsif number_match?(guess, index)
-        mark << '-'
-      end
+      @output.puts '+'*exact_match_count(guess) +  '-'*number_match_count(guess)
     end
 
-    @output.puts mark
-  end
+    def number_match_count(guess)
+      number_match_count = 0
+      (0..3).each do |index|
+        if number_match?(guess, index)
+          number_match_count += 1
+        end
+      end
+      number_match_count
+    end
 
-  def number_match?(guess, index)
-    @secret.include?(guess[index])
-  end
+    def exact_match_count(guess)
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
+    end
 
-  def exact_match?(guess, index)
-    guess[index] == @secret[index]
-  end
+    def number_match?(guess, index)
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
+    end
 
-end
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+  end
 end
